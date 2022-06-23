7945b6ceab726266f2c24048a2efd224849d80f2
emmanueljaricha <manujaricha@gmail.com>
2022-06-23 23:53:57 +0200

    Calculator First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function display(value) 
+{
+    document.getElementById('textarea').value+=value //appending
+}
+
+function calculate() 
+{
+    let value = document.getElementById('textarea').value
+    let answer = eval(value)
+    document.getElementById('textarea').value = answer
+}
+
+function clr(){
+    document.getElementById('textarea').value = '';
+}
